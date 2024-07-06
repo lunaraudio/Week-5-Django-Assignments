6bfc921c6cc6b1be304da38aa0c14d4dd90bd207
lunaraudio <164691587+lunaraudio@users.noreply.github.com>
2024-07-06 16:10:26 -0400

    Add files via upload

---
+++ mysite.py
@@ -0,0 +1,12 @@
+""" mysite URL configuration
+
+[...]
+"""
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
+]
